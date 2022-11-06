fc56fbabeefd2c77910590876b43dfbec7705461
Paulo Cesar Souza Pereira <drlund@hotmail.com>
2022-11-06 20:22:14 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Imagem from './img/people.svg'
 
 function App() {
+
+  const [comentario, setComentario] = useState('');
+  const [todosOsComentarios, setTodosOsComentarios] = useState([])
+  
+  // Salva o texto dentro da função "setComentario" que atribui o estado à variável "comentario"
+  // Os valores estão dentro de comentario e são passados para "setTodosOsComentarios" para o evento do botão
+  function typeTextarea(event){
+    setComentario(event.target.value);
+  }
+
+  function clickButton(){
+    const todosOsComentariosAnteriores = [...todosOsComentarios, comentario];
+    setTodosOsComentarios(todosOsComentariosAnteriores)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <container>
+      <img src={Imagem} alt='imagem-pessoas' />
+      <textarea onChange={typeTextarea}></textarea>
+      <button onClick={clickButton} type='text'>Comentar</button>
+      <ul>
+        {todosOsComentarios.map(coment => (
+          <li key="coment">{coment}</li>
+        ))}
+      </ul>
+      </container>
+
     </div>
   );
 }
